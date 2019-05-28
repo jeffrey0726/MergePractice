8e2d1b144d13b9630f62d9e5aefd0fd3900451ee
jeffrey0726 <angulin726@gmail.com>
2019-05-28 11:30:06 +0800

    randselect & course

--- RandomSelect/main.js
+++ RandomSelect/main.js
@@ -5,6 +5,13 @@ $(document).ready(function(){
         var randomChildNumber = Math.floor(Math.random()*numberOfListItem);
         
         $("H1").text($("#choices li").eq(randomChildNumber).text());
-        $("#image").attr("src");
+        var src = new Array(5);
+        src[0] = "https://cw1.tw/CW/images/article/201702/article-58a50e8280597.jpg";
+        src[1] = "https://cdn.walkerland.com.tw/images/upload/subject/b8efcae2b3156f893af49e40cb56f38ee2c6a345.jpg";
+        src[2] = "https://tshop.r10s.com/ff1/509/d51e/03a9/70e2/a6f3/687b/11a8e682fd2c600ce3781e.jpg";
+        src[3] = "https://i.ytimg.com/vi/CJaAnZz2owo/maxresdefault.jpg";
+        src[4] = "https://tokyo-kitchen.icook.network/uploads/recipe/cover/177276/large_5d8da49a47789a20.jpg";
+        
+        $("img").attr("src",src[randomChildNumber]);
     })
 });
--- js-class2/topic.js
+++ js-class2/topic.js
@@ -6,19 +6,18 @@ var topic = [
     "不上課"
 ]
 
-　startDate = new Date();
- function setMonthAndDate(startMonth, startDay)
+startDate = new Date();
+function setMonthAndDate(startMonth, startDay)
 {
     startDate.setMonth(startMonth-1, startDay);
     startDate.setHours(0);
     startDate.setMinutes(0);
     startDate.setSeconds(0);
 }
-
-var form = document.getElementById("start");
-var date = form.tripstart.value;
-var item = new Array();
-item = String.split("-");
-setMonthAndDate(item[1], item[2]);
-
-//setMonthAndDate(3, 30);
+/*function changeDate()
+{
+    var date = $("#start").val();
+    var item = date.split('-');
+    setMonthAndDate(item[1].replace(/^0/,''), item[2].replace(/^0/,''));   
+}*/
+setMonthAndDate(3, 30);
